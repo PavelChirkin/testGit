547fcd02d535484bf2e04e12e4f8dacaf979d3d6
PavelChirkin <pavel.chirkin@gmail.com>
2021-07-03 16:10:45 +0300

    two

---
+++ src/Package_Git/Hello.java
@@ -0,0 +1,9 @@
+package Package_Git;
+
+public class Hello {
+    public static void main(String[] args) {
+// generate some simple output
+        System.out.println("Hello, World! Hurray");
+
+    }
+}
