1b089946703acf92caf8b21b4fb7b00f0a259d8a
PavelChirkin <pavel.chirkin@gmail.com>
2021-08-18 18:53:20 +0300

    1 change

--- src/Package_Git/Hello.java
+++ src/Package_Git/Hello.java
@@ -3,8 +3,8 @@ package Package_Git;
 public class Hello {
     public static void main(String[] args) {
 // generate some simple output
+        System.out.println("Hello, World!");
+        System.out.println("Cia vel as");
 
-        System.out.println("Hello, World! Hurray");
-        System.out.println("Hello, World! Again");
     }
 }
